1ead4b30da088af3d069926388887d62dd2ee4c8
Brian Schmeling <brianschmeling2@gmail.com>
2022-07-13 11:56:31 -0400

    Start creating scheduling component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import ScheduleCreate from "./Components/ScheduleCreate";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header></header>
+      <main>
+        <Routes>
+          <Route path="/" element={<ScheduleCreate />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;

c9d49837ae27dd700effe1e6afdf0c32e582873a
Brian Schmeling <brianschmeling2@gmail.com>
2022-07-19 12:03:14 -0400

    Add clock in/out buttons, and update homempage CSS

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -27,7 +27,19 @@ class Home extends Component {
     return (
       <div>
         <Nav />
-        <div>{this.state.dateTime}</div>
+        <div className="my-8 countdown font-mono text-5xlgrid grid-flow-col gap-5 text-center auto-cols-max">
+          <div className="flex flex-col p-2 bg-neutral rounded-box text-neutral-content countdown font-mono text-5xl">
+            {this.state.dateTime}
+          </div>
+        </div>
+        <div className="flex items-center justify-center my-16">
+          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Clock in
+          </button>
+          <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Clock out
+          </button>
+        </div>
       </div>
     );
   }

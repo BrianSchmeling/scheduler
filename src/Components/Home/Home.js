e50ac66675c76b107bb914066fc36d192a88960f
Brian Schmeling <brianschmeling2@gmail.com>
2022-07-19 11:37:15 -0400

    Add timer for timeclock

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
+import { useState, useEffect } from "react";
 import Nav from "../NavBar/NavBar";
 
 class Home extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      dateTime: new Date().toLocaleString(),
+    };
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+  tick() {
+    this.setState({
+      dateTime: new Date().toLocaleString(),
+    });
   }
 
   render() {
     return (
       <div>
         <Nav />
+        <div>{this.state.dateTime}</div>
       </div>
     );
   }
